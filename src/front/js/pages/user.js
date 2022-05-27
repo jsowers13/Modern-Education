c6fc7028d281565c2fbf06bec408b385b6569a2d
jsowers13 <jsowers13@gmail.com>
2022-05-27 02:00:08 +0000

    created basic school card

--- src/front/js/pages/user.js
+++ src/front/js/pages/user.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Search } from "../component/search";
+import { SchoolCard } from "../component/schoolcard";
 
 export const User = (props) => {
   const { store, actions } = useContext(Context);
@@ -14,6 +15,14 @@ export const User = (props) => {
       <br></br>
       <Search />
       <br></br>
+      <SchoolCard
+        name="4 Geeks Academy"
+        pic_url="https://www.beaconcouncil.com/wp-content/uploads/2018/08/4_geeks_academy_logo-300x150.jpg"
+        state="Online"
+        tuition="cheap"
+        time_to_complete="almost nothing"
+        id="0"
+      />
       <Link to="/">
         <span className="btn btn-primary btn-lg" href="#" role="button">
           Back home
