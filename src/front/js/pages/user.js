d4b31c01f69c0a33a755bc27ed8783dbb2fbf5c0
jsowers13 <jsowers13@gmail.com>
2022-05-28 00:13:35 +0000

    improved login

--- src/front/js/pages/user.js
+++ src/front/js/pages/user.js
@@ -4,30 +4,36 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Search } from "../component/search";
 import { SchoolCard } from "../component/schoolcard";
+import { Login } from "./login";
 
 export const User = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
-
   return (
-    <div className="container d-flex flex-column align-items-center mb-5">
-      <p>Create User View Here {store.demo[params.theid].title}</p>
-      <br></br>
-      <Search />
-      <br></br>
-      <SchoolCard
-        name="4 Geeks Academy"
-        pic_url="https://www.beaconcouncil.com/wp-content/uploads/2018/08/4_geeks_academy_logo-300x150.jpg"
-        state="Online"
-        tuition="cheap"
-        time_to_complete="almost nothing"
-        id="0"
-      />
-      <Link to="/">
-        <span className="btn btn-primary btn-lg" href="#" role="button">
-          Back home
-        </span>
-      </Link>
+    <div>
+      {!store.activeUser ? (
+        <Login />
+      ) : (
+        <div className="container d-flex flex-column align-items-center mb-5">
+          <p>Create User View Here </p>
+          <br></br>
+          <Search />
+          <br></br>
+          <SchoolCard
+            name="4 Geeks Academy"
+            pic_url="https://www.beaconcouncil.com/wp-content/uploads/2018/08/4_geeks_academy_logo-300x150.jpg"
+            state="Online"
+            tuition="cheap"
+            time_to_complete="almost nothing"
+            id="0"
+          />
+          <Link to="/">
+            <span className="btn btn-primary btn-lg" href="#" role="button">
+              Back home
+            </span>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
