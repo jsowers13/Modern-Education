144e9f1eb76bc419da7b8e8b469507a8c2e4ed0a
bashh69 <bashh@hotmail.com>
2022-06-14 01:13:14 +0000

    luisito was here

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export const Footer = () => (
   <footer className="footer container-fluid bg-light position-fixed bottom-0 start-0 mt-auto py-3 text-center">
     <p>
-      Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+      Made with <i className="fa fa-clock text-success" /> by{" "}
+      <a href="http://www.4geeksacademy.com">The good guys</a>
     </p>
   </footer>
 );
