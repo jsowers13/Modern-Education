4a0798c5a1057cf9abb9dc2e68fd8effff4c343d
jsowers13 <jsowers13@gmail.com>
2022-06-15 23:47:43 +0000

    began building search tabs

--- src/front/js/component/search.js
+++ src/front/js/component/search.js
@@ -2,146 +2,58 @@ import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
 import { SchoolCard } from "./schoolcard.js";
+import { BootcampSearch, CollegeSearch } from "./searchBoxes";
 
 export const Search = () => {
-  const { store, actions } = useContext(Context);
-  const [skillLevelValue, setSkillLevelValue] = useState("");
-  const [programType, setProgramType] = useState("");
-  const [maxTuitionValue, setMaxTuitionValue] = useState(50000);
-  const [timeToCompleteValue, setTimeToCompleteValue] = useState(99);
-  const [searchResults, setSearchResults] = useState([]);
-  const [schoolTypeValue, setSchoolTypeValue] = useState("");
-  const searchFunction = () => {
-    let newResults = store.schools.filter(
-      (item) =>
-        // item.minimum_skill_level === skillLevelValue &&
-        item.career_options.includes(programType) &&
-        item.tuition <= maxTuitionValue &&
-        item.length_in_weeks <= timeToCompleteValue
-
-      // item.minimum_skill_level === skillLevelValue &&
-      // item.career_options.includes(programType) &&
-      // item.tuition <= maxTuitionValue
-      // item.length_in_weeks <= timeToCompleteValue
-    );
-
-    setSearchResults(newResults);
-
-    console.log(newResults);
-  };
-  const searchColleges = () => {
-    let newResults = store.colleges.filter(
-      (item) =>
-        // item.minimum_skill_level === skillLevelValue &&
-        item.career_options.includes(programType) &&
-        item.tuition <= maxTuitionValue &&
-        item.length_in_weeks <= timeToCompleteValue
-
-      // item.minimum_skill_level === skillLevelValue &&
-      // item.career_options.includes(programType) &&
-      // item.tuition <= maxTuitionValue
-      // item.length_in_weeks <= timeToCompleteValue
-    );
-
-    setSearchResults(newResults);
-
-    console.log(newResults);
-  };
-  const savedSchools = store.activeUser.favorites;
   return (
-    <div className="text-center container-fluid">
-      <div className="card mx-auto my-5" style={{ width: 24 + "rem" }}>
-        <div className="card-body">
-          <h3 className="card-title">Find Your School!</h3>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <h5>Type of School</h5>
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            placeholder="Type of School"
-            id="schoolDropdown"
-            onChange={(e) => setSchoolTypeValue(e.target.value)}
-            value={schoolTypeValue}
+    <div>
+      <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active rounded-pill"
+            id="pills-bootcamp-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-bootcamp"
+            type="button"
+            role="tab"
+            aria-controls="pills-bootcamp"
+            aria-selected="true"
           >
-            <option defaultValue={"Bootcamp"}></option>
-            <option value="Bootcamp">Bootcamp</option>
-            <option value="College">College</option>
-          </select>
-          <h5>Type of Program</h5>
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            placeholder="Select from List"
-            id="programDropdown"
-            onChange={(e) => setProgramType(e.target.value)}
-            value={programType}
+            Search for BootCamps
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link rounded-pill"
+            id="pills-college-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-college"
+            type="button"
+            role="tab"
+            aria-controls="pills-college"
+            aria-selected="false"
           >
-            <option defaultValue={""}></option>
-            <option value="Coding">Coding</option>
-            <option value="Software Development">Software Development</option>
-            <option value="Data Analytics">Data Analytics</option>
-            <option value="Data Science">Data Science</option>
-            <option value="Digital Marketing">Digital Marketing</option>
-            <option value="UX/UI Design">UX/UI Design</option>
-            <option value="Cyber Security">Cyber Security</option>
-            <option value="Machine Learning">Machine Learning</option>
-            <option value="FinTech">FinTech</option>
-          </select>
-
-          <h5>Max Tuition</h5>
-          <input
-            className="form-control"
-            type="text"
-            aria-label="Default select example"
-            placeholder=""
-            id="tuitionDropdown"
-            onChange={(e) => setMaxTuitionValue(e.target.value)}
-          ></input>
-          <h5>Max Length</h5>
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            placeholder="Time to Complete"
-            id="timeToCompleteDropdown"
-            onChange={(e) => setTimeToCompleteValue(e.target.value)}
-            value={timeToCompleteValue}
-          >
-            <option defaultValue={99}></option>
-            <option value={12}>12 Weeks</option>
-            <option value={16}>16 Weeks</option>
-            <option value={24}>24 Weeks</option>
-            <option value={32}>32 Weeks</option>
-          </select>
+            Search for Colleges
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="pills-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="pills-bootcamp"
+          role="tabpanel"
+          aria-labelledby="pills-bootcamp-tab"
+        >
+          <BootcampSearch />
         </div>
-
-        <div className="card-body d-grid">
-          <a
-            href="#"
-            className="card-link btn btn-lg btn-success col-9 mx-auto"
-            onClick={searchFunction}
-          >
-            Search
-          </a>
+        <div
+          className="tab-pane fade"
+          id="pills-college"
+          role="tabpanel"
+          aria-labelledby="pills-college-tab"
+        >
+          <CollegeSearch />
         </div>
-      </div>
-      <div className="card-deck-wrapper">
-        {searchResults.map((item, index) => {
-          return (
-            <SchoolCard
-              school_name={item.school_name}
-              pic_url={item.logo}
-              tuition={item.tuition}
-              skill_level={item.minimum_skill_level}
-              time_to_complete={item.length_in_weeks + " weeks"}
-              key={index}
-              index={index}
-              id={item.school_id}
-            />
-          );
-        })}
       </div>
     </div>
   );
