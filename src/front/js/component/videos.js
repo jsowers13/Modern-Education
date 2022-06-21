63f20de6f527350ebcb95fb251a29bf5ffd5f96d
bashh69 <bashh@hotmail.com>
2022-06-21 00:33:20 +0000

    videos

--- src/front/js/component/videos.js
+++ src/front/js/component/videos.js
@@ -4,12 +4,48 @@ export const Video = () => {
   return (
     <div>
       <iframe
-        src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-        frameborder="0"
+        src="https://www.youtube.com/embed/t6wZR2P-ZqA"
+        frameborder="10"
         allow="autoplay; encrypted-media"
         allowfullscreen
         title="video"
-      />{" "}
+        width="400"
+        height="315">
+      </iframe>
+      <iframe
+        src="https://www.youtube.com/embed/-dYwGxLulOY"
+        frameborder="10"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+        width="400"
+        height="315">
+      </iframe><iframe
+        src="https://www.youtube.com/embed/34sxyECkEkw
+        "
+        frameborder="10"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+        width="400"
+        height="315">
+      </iframe><iframe
+        src="https://www.youtube.com/embed/u1lZ9FN2Mxs"
+        frameborder="10"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+        width="400"
+        height="315">
+      </iframe><iframe
+        src="https://www.youtube.com/embed/L6_Q9ZkjeEk"
+        frameborder="10"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+        width="400"
+        height="315">
+      </iframe>{" "}
     </div>
   );
-}
+};
