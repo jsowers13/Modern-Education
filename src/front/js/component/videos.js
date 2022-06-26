91c359d7aab10a4d74ffb3264ccbded6d067dab4
bashh69 <bashh@hotmail.com>
2022-06-26 16:52:21 +0000

    video positioning

--- src/front/js/component/videos.js
+++ src/front/js/component/videos.js
@@ -3,51 +3,55 @@ import "../../styles/home.css";
 
 export const Video = () => {
   return (
-    <div className="mt-1 bg-transparent">
-      <iframe 
-        src="https://www.youtube.com/embed/t6wZR2P-ZqA"
-        frameborder="10"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-        width="400"
-        height="315"
-           >
-      </iframe>
-      <iframe
-        src="https://www.youtube.com/embed/-dYwGxLulOY"
-        frameborder="10"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-        width="400"
-        height="315">
-      </iframe><iframe
-        src="https://www.youtube.com/embed/34sxyECkEkw
+    <div className="container">
+      <div className="mb-1 bg-transparent row-md-2">
+        <iframe 
+          src="https://www.youtube.com/embed/t6wZR2P-ZqA"
+          frameborder="10"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          width="400"
+          height="315"
+        ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/-dYwGxLulOY"
+          frameborder="10"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          width="400"
+          height="315"
+        ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/34sxyECkEkw
         "
-        frameborder="10"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-        width="400"
-        height="315">
-      </iframe><iframe
-        src="https://www.youtube.com/embed/u1lZ9FN2Mxs"
-        frameborder="10"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-        width="400"
-        height="315">
-      </iframe><iframe 
-        src="https://www.youtube.com/embed/L6_Q9ZkjeEk"
-        frameborder="10"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-        width="400"
-        height="315">
-      </iframe>{" "}
+          frameborder="10"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          width="400"
+          height="315"
+        ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/u1lZ9FN2Mxs"
+          frameborder="10"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          width="400"
+          height="315"
+        ></iframe>
+        <iframe
+          src="https://www.youtube.com/embed/L6_Q9ZkjeEk"
+          frameborder="10"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+          width="400"
+          height="315"
+        ></iframe>{" "}
+      </div>
     </div>
   );
 };
